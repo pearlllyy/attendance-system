276ee2856467f35bf57079fa97a280a297ffda50
Pearl <sobrevega.pearlanne.g@gmail.com>
2026-05-18 10:11:46 +0800

    added git ignore

---
+++ config_example.py
@@ -0,0 +1,11 @@
+class Config:
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'your_password_here'
+    MYSQL_DB = 'attendance_db'
+    MYSQL_PORT = 3306
+
+    SECRET_KEY = 'your_secret_key_here'
+
+    CUTOFF_HOUR = 8
+    CUTOFF_MINUTE = 0
