87e721274288203a81dc258eea29a39bfd4fc79a
euigwapo <bacarisaseuijin@proton.me>
2026-07-12 10:56:51 +0800

    flask debug false for security

--- app.py
+++ app.py
@@ -957,4 +957,4 @@ def delete_log():
 
 # ─── Run ──────────────────────────────────────────────────────────
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context='adhoc')
+    app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')
